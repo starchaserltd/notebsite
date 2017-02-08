96a3d15fef64a80ac765efff13915d43be9f92f9
root <root@ip-172-31-8-200.ec2.internal>
2017-02-08 13:38:43 +0000

    title index

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require_once("etc/session.php");
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Noteb.com</title>
+	<title>Noteb</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="description" content="World largest notebook and laptop information hub.">
