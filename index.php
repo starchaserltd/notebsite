7ed0bdf388eef645064022e5ec90f45f91433e0b
root <root@static-86-123-134-36.rdsnet.ro>
2017-08-03 13:04:54 +0300

    critical bug

--- index.php
+++ index.php
@@ -176,6 +176,7 @@ require_once("etc/session.php");
 	<link rel="stylesheet" href="lib/css/bootstrap-theme.min.css" type="text/css" />
 	<link rel="stylesheet" href="lib/css/select2.min.css" type="text/css" /> 
 	<link rel="stylesheet" href="lib/css/bootstrap-multiselect.css" type="text/css" />
+	 <link rel="stylesheet" href="search/quiz/quiz.css" type="text/css" />
 	<!-- for multiselect forms -->  
 	<link rel="stylesheet" href="lib/css/extra-fonts.css" type="text/css" />
 	<!-- menu fonts -->
@@ -183,7 +184,7 @@ require_once("etc/session.php");
 	<link rel="stylesheet" href="lib/css/bootstrap-social.css" type="text/css" />
 	<!-- socicon, bootstrap social - css for social media-->
 	<link rel="stylesheet" href="lib/css/nouislider.min.css" type="text/css"/>
-	<link rel="stylesheet" href="http://86.123.134.36/notebro/lib/css/nouislider.min.css" type="text/css" />
+	<link rel="stylesheet" href="lib/css/nouislider.min.css" type="text/css" />
 	<link rel="stylesheet" href="libnb/css/nb.css" type="text/css" />
 	<!--custom OK	-->
 	<link rel="stylesheet" href="libnb/css/responsive.css" type="text/css" />
@@ -192,8 +193,9 @@ require_once("etc/session.php");
 	<!-- Custom Theme JavaScript -->
 	<script><?php echo 'var siteroot = "'.$web_address.'";'; ?></script>
 	<script type="text/javascript" src="libnb/js/tooltip.js" async></script>
+	<script type="text/javascript" src="search/quiz/classList.min.js"></script>
 	<script type="text/javascript" src="libnb/js/index.js"></script>
-	<script type="text/javascript" src="http://86.123.134.36/notebro/lib/js/nouislider.min.js"></script>
+	<script type="text/javascript" src="lib/js/nouislider.min.js"></script>
 	
 </body>
 </html>
