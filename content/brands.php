fbbb17b3b24c1158fc62cfe40f2a67268aa227ff
root <root@static-86-123-134-36.rdsnet.ro>
2017-08-09 17:21:12 +0300

    brands

--- content/brands.php
+++ content/brands.php
@@ -13,7 +13,7 @@ while($row = mysqli_fetch_array($results))
 {
 	echo '<a style="cursor:pointer" onmousedown="OpenPage('."'".'search/search.php?prod='.$row["brand"].'&browse_by=prod'."'".',event)"><div class="col-md-2 col-sm-2 col-xs-6 col-lg-2">
 	<div style="margin-right:5px; margin-bottom: 5px; padding:5px; font-size:15px; text-align:center; max-width:120px;">
-	<div class="brands" ><img class="img-responsive" style= "display:block;max-width:100%;height:auto;width:100%; vertical-align: middle;" src="res/'.$row["pic"].'"></div><div  style="color:#000; font-weight:600; text-align:center;">'.$row["brand"].'</div>
+	<div class="brands" ><img class="img-responsive" style= "display:block;max-width:100%;height:auto;width:100%; vertical-align: middle;" src="res/'.$row["pic"].'" alt="Brand Logo"></div><div  style="color:#000; font-weight:600; text-align:center;">'.$row["brand"].'</div>
 	</div></div></a>';
 }
 ?>
