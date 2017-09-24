4f9b417b2ec015f755cedd120227543721657344
root <root@static-86-123-134-36.rdsnet.ro>
2017-09-25 01:18:48 +0300

    https

--- libnb/php/urlproc.php
+++ libnb/php/urlproc.php
@@ -8,7 +8,8 @@ function url_origin( $s, $use_forwarded_host = false )
     $port     = ( ( ! $ssl && $port=='80' ) || ( $ssl && $port=='443' ) ) ? '' : ':'.$port;
     $host     = ( $use_forwarded_host && isset( $s['HTTP_X_FORWARDED_HOST'] ) ) ? $s['HTTP_X_FORWARDED_HOST'] : ( isset( $s['HTTP_HOST'] ) ? $s['HTTP_HOST'] : null );
     $host     = isset( $host ) ? $host : $s['SERVER_NAME'] . $port;
-    return $protocol . '://' . $host;
+	$protocol=str_ireplace("http",$port_type,$protocol);
+	return $protocol . '://' . $host;
 }
 
 function full_url( $s, $use_forwarded_host = false )
