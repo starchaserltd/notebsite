b331781053a7c0d444a47b7b475ec526e49fe035
root <root@static-86-123-134-36.rdsnet.ro>
2017-09-25 01:44:25 +0300

    sslul vietii

--- libnb/php/urlproc.php
+++ libnb/php/urlproc.php
@@ -2,13 +2,13 @@
 function url_origin( $s, $use_forwarded_host = false )
 {
     $ssl      = ( ! empty( $s['HTTPS'] ) && $s['HTTPS'] == 'on' );
-    $sp       = strtolower( $s['SERVER_PROTOCOL'] );
+	if(!$ssl) { if($GLOBALS["port_type"]==="https") {$ssl=true;} }
+	$sp       = strtolower( $s['SERVER_PROTOCOL'] );
     $protocol = substr( $sp, 0, strpos( $sp, '/' ) ) . ( ( $ssl ) ? 's' : '' );
     $port     = $s['SERVER_PORT'];
     $port     = ( ( ! $ssl && $port=='80' ) || ( $ssl && $port=='443' ) ) ? '' : ':'.$port;
     $host     = ( $use_forwarded_host && isset( $s['HTTP_X_FORWARDED_HOST'] ) ) ? $s['HTTP_X_FORWARDED_HOST'] : ( isset( $s['HTTP_HOST'] ) ? $s['HTTP_HOST'] : null );
     $host     = isset( $host ) ? $host : $s['SERVER_NAME'] . $port;
-	$protocol=str_ireplace("http",$port_type,$protocol);
 	return $protocol . '://' . $host;
 }
 
