13087a1f593e5db7d5fd29b43d29dfa021a5f2a2
root <root@static-86-123-134-36.rdsnet.ro>
2017-11-16 15:35:11 +0200

    varza

--- search/results.php
+++ search/results.php
@@ -168,8 +168,8 @@ while($row=mysqli_fetch_array($result)){ $regions[$row[0]]=$row[1]; }
 ?>
 <script type="text/javascript"> excode='<?php echo $_SESSION['exchcode']; ?>'; document.title = 'Noteb - Search results';
 $.getScript("../lib/js/jquery.matchHeight-min.js").done(function(){ $.getScript("search/lib/js/results.js"); });
-<?php 
-if (isset($browse_by))
+<?php
+if (isset($browse_by)&&$browse_by!==0)
 { ?>
 	var el=document.getElementById("SearchParameters");
 	if(!(el.classList.contains("showMoreDetails"))){ el.classList.add("showMoreDetails");  document.getElementsByClassName("leftMenuFilters")[0].classList.add("rotate"); }
