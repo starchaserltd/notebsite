78d23270f950c8ad661450af11327ebdd47fe70c
root <root@static-86-123-134-36.rdsnet.ro>
2018-03-18 16:22:29 +0200

    v 1.085

---
+++ api/lib/api_search.php
@@ -0,0 +1,54 @@
+<?php
+$query_search="";
+$orderby = "ORDER BY rating ASC";
+$orderby_index = "USE INDEX FOR ORDER BY (rating)";
+$sort_func = "sort_func_by_rating";
+
+$relativepath="";
+require_once("../etc/con_sdb.php");
+require_once("../etc/conf.php");
+require_once("preproc/api_varproc.php");
+
+if(!$abort)
+{
+	ob_start();
+	require("../search/proc/search_filters.php");
+	ob_get_clean();
+
+	$conds = array();
+	foreach (array("cpu", "display", "gpu", "acum", "war", "hdd", "wnet", "sist", "odd", "mem", "mdb", "chassis") as $v) 
+	{
+		if (!$to_search[$v]) { continue; }
+		$conds[$v] = $v . " IN (" . implode(",", array_keys($comp_lists[$v])) . ")";
+	}
+
+	if ($nr_hdd > 1) { $conds["shdd"]="shdd > 0"; }
+	$conds["capacity"] = "(capacity BETWEEN " . $totalcapmin . " AND " . $totalcapmax . " )";
+	$results = array(); $queries = array();
+
+	if(!isset($comp_lists["model"])){ $comp_lists["model"]=$comp_lists_api["model"]; }
+	foreach($comp_lists["model"] as $m)
+	{
+		$model = $m["id"];
+		$conds_model = $conds;
+		
+		if($conds_model)
+		{ $query_search = "SELECT id FROM notebro_temp.all_conf_".$model." WHERE " . implode(" AND ", $conds_model) . " AND price>0 ORDER BY value DESC LIMIT 1"; }
+
+		/* DEBUGGING CODE */
+		#echo "<pre>" . var_dump($query_search) . "</pre>";
+
+		$result=mysqli_query($cons, $query_search);
+		if($result)
+		{ 
+			$result = mysqli_fetch_assoc($result); 
+			if ((!is_null($result)) && ($result["id"]!=NULL))
+			{ 
+				$idmodel = mysqli_fetch_row(mysqli_query($con,"SELECT model FROM notebro_temp.all_conf WHERE id = ".$result['id'].""));
+				require("model_param.php");
+				if($single_result){	break; }
+			}
+		}
+	}
+}
+?>
