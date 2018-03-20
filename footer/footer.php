d1d36f5e080352db21f423ea121f26262c353067
Cloud User <centos@ip-172-31-8-200.ec2.internal>
2018-03-20 08:13:09 +0000

    something stupid

--- footer/footer.php
+++ footer/footer.php
@@ -1,8 +1,8 @@
 	<footer class="footer-distributed">
 		<div class="footer-right">
 			<a style="cursor:pointer" href="https://www.facebook.com/Noteb-162949570886382/"><i class="socicon socicon-facebook"></i></a>
-			<a style="cursor:pointer" href="https://twitter.com/notebcom"><i class="socicon socicon-twitter"></i></a>
-			
+			<a style="cursor:pointer" href="https://twitter.com/notebrother"><i class="socicon socicon-twitter"></i></a>
+			<a style="cursor:pointer" href=""><i class="socicon socicon-google-plus"></i></a>
 			
 		</div>
 		<div class="footer-left">
@@ -17,24 +17,3 @@
 			</div>
 	</footer>
 <link rel="stylesheet" href="../lib/css/footer-distributed.css" type="text/css" />
-<script type="text/javascript">
-    adroll_adv_id = "LVH53BDVFBGEJHPR6XEVJI";
-    adroll_pix_id = "CBQN4EWN5FC7REZ6OWCLEW";
-    /* OPTIONAL: provide email to improve user identification */
-    /* adroll_email = "username@example.com"; */
-    (function () {
-        var _onload = function(){
-            if (document.readyState && !/loaded|complete/.test(document.readyState)){setTimeout(_onload, 10);return}
-            if (!window.__adroll_loaded){__adroll_loaded=true;setTimeout(_onload, 50);return}
-            var scr = document.createElement("script");
-            var host = (("https:" == document.location.protocol) ? "https://s.adroll.com" : "http://a.adroll.com");
-            scr.setAttribute('async', 'true');
-            scr.type = "text/javascript";
-            scr.src = host + "/j/roundtrip.js";
-            ((document.getElementsByTagName('head') || [null])[0] ||
-                document.getElementsByTagName('script')[0].parentNode).appendChild(scr);
-        };
-        if (window.addEventListener) {window.addEventListener('load', _onload, false);}
-        else {window.attachEvent('onload', _onload)}
-    }());
-</script>
