044b061f7f3149875c61ba0a4e2779475bec378d
root <root@static-86-123-134-36.rdsnet.ro>
2018-10-12 17:01:00 +0300

    v 1.646

--- search/proc/cpu_search.php
+++ search/proc/cpu_search.php
@@ -237,14 +237,14 @@ function search_cpu ($prod, $model, $ldmin, $ldmax, $status, $socket, $techmin, 
 	if(gettype($misc)!="array") { $misc=(array)$misc; }
 	foreach($misc as $x)
 	{
-		if(stripos($x,"Intel Core i")!==FALSE)
+		if(stripos($x,"Intel Core i")!==FALSE||stripos($x,"Intel Xeon")!==FALSE)
 		{
 			if($i)
 			{ $sel_cpu.=" OR "; }
 			else
 			{ $sel_cpu.=" AND ("; }
 			
-			$x=str_ireplace("Intel Core ","",$x);
+			if(stripos($x,"Xeon")!==FALSE){$x="Xeon";}else{$x=str_ireplace("Intel Core ","",$x);}
 			if(stripos($x,"/")!==FALSE)
 			{ $x=explode("/",$x); $sel_cpu.="(model LIKE '%".$x[0]."%' OR model LIKE '%".$x[1]."%')"; }
 			else
@@ -299,7 +299,7 @@ function search_cpu ($prod, $model, $ldmin, $ldmax, $status, $socket, $techmin, 
 	// DO THE SEARCH
 	# echo "Query to select the CPUs:";
     # echo "<br>";
-	#echo "<pre>" . $sel_cpu . "</pre>";
+	# echo "<pre>" . $sel_cpu . "</pre>";
 
 	$result = mysqli_query($GLOBALS['con'], "$sel_cpu");
 	$cpu_return = array();
