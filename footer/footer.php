7db76b085e957f81b23ff908149a0690cbba7a01
root <root@static-86-123-134-36.rdsnet.ro>
2018-10-15 11:50:02 +0300

    v 1.648

--- footer/footer.php
+++ footer/footer.php
@@ -2,7 +2,7 @@
 		<div class="footer-right">
 			<a style="cursor:pointer" href="https://www.facebook.com/Noteb-162949570886382/"><i class="socicon socicon-facebook"></i></a>
 			<a style="cursor:pointer" href="https://twitter.com/notebcom"><i class="socicon socicon-twitter"></i></a>
-			
+			<a style="cursor:pointer" href="https://www.youtube.com/channel/UC7nnclcN7BZDn_FqKtzwBGQ"><i class="socicon socicon-youtube"></i></a>
 			
 		</div>
 		<div class="footer-left">
