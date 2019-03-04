e166fb55e6165bc3a87d17d18c6e91d1000a4f10
root <root@static-86-123-134-36.rdsnet.ro>
2019-03-04 09:38:55 +0200

    v 1.816

--- public/lib/php/queries.php
+++ public/lib/php/queries.php
@@ -21,7 +21,7 @@ switch($select)
 	}
 	case "com_info":
 	{
-		$query="SELECT DISTINCT `model`,`comment` FROM `notebro_db`.`COMMENTS` WHERE type='com' AND model='".$keys."' AND valid=1 LIMIT 1";
+		$query="SELECT DISTINCT `model`,`comment` FROM `notebro_db`.`COMMENTS` WHERE type='com' AND (model='".$keys."' OR model=(SELECT `p_model` FROM `notebro_db`.`MODEL` WHERE `notebro_db`.`MODEL`.`id`='".$keys."' LIMIT 1)) AND `update`=0 LIMIT 1";
 		$result=mysqli_query($con,$query);
 		$i=1;
 		while($rand = mysqli_fetch_row($result)) { $list[]=["model"=>$rand[0],"comment"=>strval($rand[1])]; $i++; }
