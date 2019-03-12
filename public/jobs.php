4ec2d240f65608b75806c36339c0b2dce03829d9
root <root@static-86-123-134-36.rdsnet.ro>
2019-03-12 15:27:15 +0200

    v 1.819

--- public/jobs.php
+++ public/jobs.php
@@ -1,8 +1,30 @@
 <!DOCTYPE html>
 <html>
 <body>
-<?php
-echo "aaa";
-?>
+<br><br>
+<div style="text-align:center;"><img width="100" height="100" src="../res/img/logo/logo_noteb.png"></div>
+<div style="text-align:center; font-size:13pt; margin: 10 10 10 10;"><p><b><a href="https://noteb.com">Noteb.com</a></b> is an innovative search engine specialised in laptops, that combines advanced analytics and machine-learning technology with a proprietary rating system to deliver a unique user experience. Similar in purpose, but more advanced than the product search pages of large retailers, Noteb guides its users to the product of their desire by using tools tailored to their expertise.</p></div>
+<br>
+<div style="text-align:center; font-size:14pt; margin: 10 10 10 10;">Our project still needs work, and we appreciate any help we can get. If you think you have the time and skills to help us on a part-time basis, don't hesitate to contact us at <a href="mailto:office@starchaser.ro?Subject=Looking%20to%20help" target="_top">office@starchaser.ro</a>.<br><br>Here is a full list of what we are currently looking for:
+<br><br><br>
+<table align="center" border="0" style="text-align:left;">
+<tr><td colspan="2" style="font-size:16pt"><b>Crawler development</b></td></tr>
+<tr><td><b>Skills:</b></td><td>Python 3.x, SQL (MariaDB)</td></tr>
+<tr><td><b>Work volume:</b></td><td>24 - 48 hours in total</td></tr>
+<tr><td><b>Status:</b></td><td>Open since 1st of March 2019</td></tr>
+<tr><td><b>Experience level:</b></td><td>Good experience with Python</td></tr>
+<tr><td><b>Description:</b></td><td width="800px;">Noteb.com has a propriety crawler to scrape the internet for laptop prices. However, the crawler currently works with most of its values predefined, ex.: the list of pages it has to read, what to select from each of them and what to do with the information. We want to make the crawler more dynamic, in a sense, that it works like a service, which receives the URL it has to read plus some other parameters like the CSS selectors and rules on what to return depending on what it finds with those selectors.</td></tr>
+</table>
+<br><br>
+<table align="center" border="0" style="text-align:left;">
+<tr><td colspan="2" style="font-size:16pt"><b>CSS bugs and improvements</b></td></tr>
+<tr><td><b>Skills:</b></td><td>CSS, Bootstrap 4</td></tr>
+<tr><td><b>Work volume:</b></td><td>12 - 24 hours in total</td></tr>
+<tr><td><b>Status:</b></td><td>Open since 1st of March 2019</td></tr>
+<tr><td><b>Experience level:</b></td><td>Some experience with CSS, minor experience with Javascript / JQuery</td></tr>
+<tr><td><b>Description:</b></td><td width="800px;">We have a list of bugs for our CSS code. Most of them are minor, but some may require a bit more work. We are looking for someone who would like to fix those bugs and maybe improve the overall CSS and HTML code.</td></tr>
+</table>
+</div>
+<?php echo ""; ?>
 </body>
 </html>
