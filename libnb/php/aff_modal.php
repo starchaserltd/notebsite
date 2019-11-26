96fa65e9dc3631926da596283f5ab57bf0aba807
root <root@static-86-123-134-36.rdsnet.ro>
2019-11-26 22:20:08 +0200

    v 1.851

--- libnb/php/aff_modal.php
+++ libnb/php/aff_modal.php
@@ -4,16 +4,17 @@
 		<div class="modal-content" id="modal-affil-content">
 			<div class="modal-header">
 				<button type="button" class="close" data-dismiss="modal" aria-label="Close" id="close-affil-btn">
-					<span aria-hidden="true">&times;</span>
+					<span aria-hidden="true"<i class="fas fa-times"></i></span>
 				</button>
 			</div>
 			<div class="modal-body" id="affil-popup-body" style="transition: all .5s linear">
-				<p><b><span>Let the stores know you came from noteb?</span></b> <br/><span>Your gratitude keeps us up and running. </span><span id="learn-more-affil-btn" style="color: #000000;"><a href="#" style="color: #000000;">Learn more</a></span></p>
+				<p><b><span style="font-weight: 900; color:#0066cb;">Let the stores know you came from noteb?</span></b> <br/><span style="color:#636363; padding-top:20px;display: inline-block;">Your gratitude keeps us up and running. </span></p>
 			</div>
 			<div class="modal-footer">
 				<button type="button" class="btn btn-primary" data-dismiss="modal" id="yes-affil-btn">Yes</button>
 				<button type="button" class="btn btn-secondary" data-dismiss="modal" id="no-affil-btn">No</button>
 			</div>
+			<span id="learn-more-affil-btn" style="color:#000000;"><a href="#" style="color:#0066cb;">Learn More</a></span>
 		</div>
 	</div>
 </div>
