3b564ffa61d89d8ca994bebc05eab6571feb9bab
root <root@static-86-123-134-36.rdsnet.ro>
2025-05-25 16:05:24 +0300

    Stop tracking machine-specific files in etc/

--- etc/errdisp.php
+++
@@ -1,7 +0,0 @@
-<?php 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-function var_error_log($object=null){ob_start(); var_dump($object); $contents=ob_get_contents(); ob_end_clean(); error_log($contents);}
-error_reporting(E_ALL);
-echo getcwd() . "\n";
-?>
