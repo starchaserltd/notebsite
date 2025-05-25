f6ecce7af64f8ffce27b6f96fa9fa959e5b4d24c
Starchaser <office@starchaser.ro>
2025-05-25 15:09:53 +0300

    Create conf.php

---
+++ etc/conf.php
@@ -0,0 +1,17 @@
+<?php
+$site_name="86.123.134.36/noteb";
+$admin_address="/../admin"; 					/* ADMIN FOLDER */
+
+$wp_rmimg="";									/* REWRITE WP IMG ADDRESS */
+$wp_address="http://86.123.134.36/notebro/admin/"; /* WORDPRESS INSTALATION ADDRESS */
+$new_wp_address="http://86.123.134.36/notebro/admin/";
+
+$web_address="http://".$site_name."/";
+$root_mod="/../notebro/noteb";
+$port_type="http";
+$underwork=0;
+$laptop_comp_list=["cpu","display","mem","hdd","shdd","gpu","mdb","wnet","odd","sist","chassis","acum","war"];
+function clean_string($string){ return preg_replace('~[\x00\x0A\x0D\x1A\x22\x27\x5C]~u', '\\\$0', filter_var($string,FILTER_SANITIZE_STRING)); }
+function table($id) { $t=explode("_",$id); $id=$t[0]; if(isset($t[1])){ $model=$t[1]; } $result=mysqli_query($GLOBALS["con"],"SELECT model FROM notebro_temp.all_conf WHERE id=$id LIMIT 1"); if($result && mysqli_num_rows($result)>0) {  $model=mysqli_fetch_row($result); mysqli_free_result($result); $t[1]=$model[0]; } else { if(isset($model)) { $result=mysqli_query($GLOBALS["con"],"SELECT best_value FROM notebro_temp.best_low_opt WHERE id_model=$model LIMIT 1"); $id=mysqli_fetch_row($result); mysqli_free_result($result);  $t[0]=$id[0]; } else { $t=NULL; } }  return $t; }
+if (session_status() == PHP_SESSION_NONE) { ini_set('session.cookie_samesite', "Lax");	ini_set('session.name', "NOTEBSESSID"); session_start(); }
+?>
