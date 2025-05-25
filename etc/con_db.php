9d65a16a0e3ba99f066fd4410101ef87218ae353
root <root@static-86-123-134-36.rdsnet.ro>
2025-05-25 16:09:29 +0300

    Restore etc/ directory

---
+++ etc/con_db.php
@@ -0,0 +1,29 @@
+<?php
+function db_connect()
+{
+   static $link;
+   $link = mysqli_init();
+
+	$user="YOUR_DB_USER;
+	$pass="YOUR_DB_PASSWORD;
+	$database="notebro_db";
+	$host="YOUR_HOST";
+	$port="13306";
+			 
+    $con = mysqli_real_connect($link, $host, $user, $pass, $database, $port);
+
+    // If connection was not successful, handle the error
+    if($con === false)
+	{
+       // Handle error - notify administrator, log to a file, show an error screen, etc.
+       return mysqli_connect_error();
+    }
+	mysqli_set_charset($link,'utf8');
+    return $link;
+}
+
+$con=db_connect();
+
+if(!defined('__DB_ROOT__')){ define('__DB_ROOT__', dirname(dirname(__FILE__))); }
+require_once(__DB_ROOT__."/libnb/php/db_utils.php");
+?>
