a28e73f36083e9eab2a7e26e95f675c65772996b
root <root@static-86-123-134-36.rdsnet.ro>
2025-07-14 16:51:09 +0300

    more fixes

--- libnb/php/checkref.php
+++ libnb/php/checkref.php
@@ -2,7 +2,7 @@
 function checkref($usertag)
 {
 	require_once("../../etc/con_db.php"); $return=0;
-	$usertag=mysqli_real_escape_string($con,filter_var($usertag, FILTER_SANITIZE_STRING));
+	$usertag=mysqli_real_escape_string($con, trim(strip_tags($usertag)));
 	$result=mysqli_query($con,"SELECT * FROM `".$GLOBALS['global_notebro_buy']."`.`TAGS` WHERE usertag='".$usertag."' LIMIT 1");
 	if($result && mysqli_num_rows($result)>0){ $return=1; }
 	return $return;
