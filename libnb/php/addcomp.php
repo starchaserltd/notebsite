9d540ba971ce9f31960dd3636460e9d0b7018d89
root <root@static-86-123-134-36.rdsnet.ro>
2025-07-14 13:20:54 +0300

    aa

--- libnb/php/addcomp.php
+++ libnb/php/addcomp.php
@@ -5,78 +5,49 @@ require_once("../../etc/con_sdb.php");
 require_once("../../etc/conf.php");
 ?>
 <?php
-if(isset($_POST['sendid'])){ $config_id = mysqli_real_escape_string($con,filter_var($_POST['sendid'], FILTER_SANITIZE_STRING)); }
+if(isset($_POST['sendid'])){ $config_id = mysqli_real_escape_string($con, trim(strip_tags($_POST['sendid']))); }
 else
 {
-	$conf_model = intval($_POST['sendmid']); $conf_cpu=intval($_POST['sendcpu']); $conf_gpu=intval($_POST['sendgpu']); $conf_disp=intval($_POST['senddisp']); $conf_hdd=intval($_POST['sendhdd']); $conf_shdd=intval($_POST['sendshdd']);
+	$conf_model=intval($_POST['sendmid']); $conf_cpu=intval($_POST['sendcpu']); $conf_gpu=intval($_POST['sendgpu']); $conf_disp=intval($_POST['senddisp']); $conf_hdd=intval($_POST['sendhdd']); $conf_shdd=intval($_POST['sendshdd']);
 	$conf_mem=intval($_POST['sendmem']); $conf_chassis=intval($_POST['sendchassis']); $conf_odd=intval($_POST['sendodd']); $conf_wnet=intval($_POST['sendwnet']); $conf_war=intval($_POST['sendwar']); $conf_sist=intval($_POST['sendsis']);
 	$conf_acum=intval($_POST['sendacum']);  $conf_mdb=intval($_POST['sendmdb']);
 	$config_id=0;
-	$conf_cpu_name= mysqli_real_escape_string($con,filter_var($_POST['sendcpuname'], FILTER_SANITIZE_STRING)); $conf_gpu_name= mysqli_real_escape_string($con,filter_var($_POST['sendgpuname'], FILTER_SANITIZE_STRING)); $conf_display_size= mysqli_real_escape_string($con,filter_var($_POST['senddissize'], FILTER_SANITIZE_STRING));
-	$conf_display_res=mysqli_real_escape_string($con,filter_var($_POST['senddisres'], FILTER_SANITIZE_STRING));	$conf_mem_info=mysqli_real_escape_string($con,filter_var($_POST['sendmeminfo'], FILTER_SANITIZE_STRING)); $conf_hdd_info=mysqli_real_escape_string($con,filter_var($_POST['sendhddinfo'], FILTER_SANITIZE_STRING));
+	$conf_cpu_name=mysqli_real_escape_string($con, trim(strip_tags($_POST['sendcpuname']))); $conf_gpu_name=mysqli_real_escape_string($con, trim(strip_tags($_POST['sendgpuname']))); $conf_display_size=mysqli_real_escape_string($con, trim(strip_tags($_POST['senddissize'])));
+	$conf_display_res=mysqli_real_escape_string($con, trim(strip_tags($_POST['senddisres']))); $conf_mem_info=mysqli_real_escape_string($con, trim(strip_tags($_POST['sendmeminfo']))); $conf_hdd_info=mysqli_real_escape_string($con, trim(strip_tags($_POST['sendhddinfo'])));
 }
 
 if($config_id)
 {
 	$t=table($config_id); $config_id=$t[0];
-	$sql="SELECT id,cpu,gpu,display,mem,hdd,model FROM `".$GLOBALS['global_notebro_sdb']."`.all_conf_".$t[1]." WHERE id=$config_id LIMIT 1";;
+	$sql="SELECT id,cpu,gpu,display,mem,hdd,model FROM `".$GLOBALS['global_notebro_sdb']."`.all_conf_".$t[1]." WHERE id=$config_id LIMIT 1";
 }
 else
 {
 	$sql="SELECT id FROM `".$GLOBALS['global_notebro_sdb']."`.all_conf_".$conf_model." WHERE cpu=".$conf_cpu." AND gpu=".$conf_gpu." AND display=".$conf_disp." AND hdd=".$conf_hdd." AND shdd=".$conf_shdd." AND acum=".$conf_acum." AND mdb=".$conf_mdb." AND mem=".$conf_mem." AND odd=".$conf_odd." AND chassis=".$conf_chassis." AND wnet=".$conf_wnet." AND war=".$conf_war." AND sist=".$conf_sist." LIMIT 1";
 }
-$cons=dbs_connect();
-$result = mysqli_query($cons,$sql);
-$row = mysqli_fetch_array($result);
-mysqli_close($cons);
+$cons=dbs_connect(); $result=mysqli_query($cons,$sql); $row=mysqli_fetch_array($result); mysqli_close($cons);
 
 if($config_id)
 {
 	$conf_model=$row["model"];
-
-	$sql2='SELECT model FROM CPU WHERE id='.$row["cpu"];
-	$result2 = mysqli_query($con,$sql2);
-	$r = mysqli_fetch_assoc($result2); 
-	$conf_cpu_name=$r["model"];
-
-	$sql2='SELECT `prod`,`name` FROM `'.$global_notebro_db.'`.`GPU` WHERE `id`='.$row["gpu"];
-	$result2 = mysqli_query($con,$sql2);
-	$r = mysqli_fetch_assoc($result2); 
-	$conf_gpu_name=$r["prod"]." ".$r["name"];
-
-	$sql2='SELECT size,hres,vres FROM DISPLAY WHERE id='.$row["display"];
-	$result2 = mysqli_query($con,$sql2);
-	$r = mysqli_fetch_assoc($result2); 
-	$conf_display_size=$r['size'];
-	$conf_display_res=$r['hres']."x".$r['vres'];
-
-	$sql2='SELECT cap,type FROM MEM WHERE id='.$row["mem"];
-	$result2 = mysqli_query($con,$sql2);
-	$r = mysqli_fetch_assoc($result2); 
-	$conf_mem_info=$r['cap']."GB ".$r['type'];
-
-	$sql2='SELECT cap,type FROM HDD WHERE id='.$row["hdd"];
-	$result2 = mysqli_query($con,$sql2);
-	$r = mysqli_fetch_assoc($result2); 
-	$conf_hdd_info=$r['cap']."GB ".$r['type'];
+	$sql2='SELECT model FROM CPU WHERE id='.$row["cpu"]; $r=mysqli_fetch_assoc(mysqli_query($con,$sql2)); $conf_cpu_name=$r["model"];
+	$sql2='SELECT `prod`,`name` FROM `'.$global_notebro_db.'`.`GPU` WHERE `id`='.$row["gpu"]; $r=mysqli_fetch_assoc(mysqli_query($con,$sql2)); $conf_gpu_name=$r["prod"]." ".$r["name"];
+	$sql2='SELECT size,hres,vres FROM DISPLAY WHERE id='.$row["display"]; $r=mysqli_fetch_assoc(mysqli_query($con,$sql2)); $conf_display_size=$r['size']; $conf_display_res=$r['hres']."x".$r['vres'];
+	$sql2='SELECT cap,type FROM MEM WHERE id='.$row["mem"]; $r=mysqli_fetch_assoc(mysqli_query($con,$sql2)); $conf_mem_info=$r['cap']."GB ".$r['type'];
+	$sql2='SELECT cap,type FROM HDD WHERE id='.$row["hdd"]; $r=mysqli_fetch_assoc(mysqli_query($con,$sql2)); $conf_hdd_info=$r['cap']."GB ".$r['type'];
 }
 
-$currentconf=array("checked" =>0, "id" => $row["id"]."_".$conf_model ,"name" => $conf_model, "model"=> $conf_model , "cpu_info" => $conf_cpu_name, "gpu_info" => $conf_gpu_name, "disp_size" => $conf_display_size, "disp_res" => $conf_display_res, "mem_info" => $conf_mem_info, "hdd_info" => $conf_hdd_info );
+$currentconf=array("checked"=>0,"id"=>$row["id"]."_".$conf_model,"name"=>$conf_model,"model"=>$conf_model,"cpu_info"=>$conf_cpu_name,"gpu_info"=>$conf_gpu_name,"disp_size"=>$conf_display_size,"disp_res"=>$conf_display_res,"mem_info"=>$conf_mem_info,"hdd_info"=>$conf_hdd_info);
 
-$ij=0; $k=0; $nrcheck=-1; $already=1;
+$ij=0;$k=0;$nrcheck=-1;$already=1;
 for($i=0;$i<=9;$i++)
 {
 	if(isset($_SESSION['conf'.$i]) && $_SESSION['conf'.$i])
-	{ 	
-		if($_SESSION['conf'.$i]["checked"])
-		{ $nrcheck++; }
-		
+	{
+		if($_SESSION['conf'.$i]["checked"]){ $nrcheck++; }
 		if($_SESSION['conf'.$i]["model"]==$currentconf["model"])
-		{ 
-			if($_SESSION['conf'.$i]["id"]!=$currentconf["id"]) 
-			{ $k++; } 
-			else
-			{ $already=0; }
+		{
+			if($_SESSION['conf'.$i]["id"]!=$currentconf["id"]){ $k++; } else { $already=0; }
 		}
 	$ij++;
 	}
@@ -84,33 +55,17 @@ for($i=0;$i<=9;$i++)
 $i=$ij;
 if($already)
 {
-	if($i>9)
-	{ echo "Maximum number of compare configurations reached!++0++0++0++max"; }
+	if($i>9){ echo "Maximum number of compare configurations reached!++0++0++0++max"; }
 	else
 	{
 		mysqli_select_db($con,$global_notebro_db);
 		$sql="SELECT families.fam, model.model, model.submodel, img_1 FROM ".$global_notebro_db.".MODEL model JOIN ".$global_notebro_db.".FAMILIES families on model.idfam=families.id WHERE model.id=".$conf_model;
-		$result = mysqli_query($con,$sql);
-		$currentconf["img"]="missing";
-		while ($row = mysqli_fetch_assoc($result)) { if(strlen($row["submodel"])>21 && !preg_match("/\(.*\)/",$row["submodel"])){ $row["submodel"]=substr($row["submodel"],0,20)."."; } $name=$row["fam"]." ".$row["model"]."".$row["submodel"]; $currentconf["img"]=$row["img_1"]; } 
-		mysqli_free_result($result);
-		$_SESSION['conf'.$i]=$currentconf;
-		$nrcheck++;
-		
-		if($nrcheck<4)
-		{ $_SESSION['conf'.$i]["checked"]=1; $checked="checked='checked'"; }
-		else
-		{ $_SESSION['conf'.$i]["checked"]=0; $checked=""; $nrcheck=4;}
-		if($k)
-		{
-			$_SESSION['conf'.$i]["name"]=$name." (".$k.")";
-			echo $name." (".$k.")++".$checked."++".$nrcheck."++".$currentconf["id"]."++".$currentconf['cpu_info']."++".$currentconf['gpu_info']."++".$currentconf['disp_size']."++".$currentconf['disp_res']."++".$currentconf['mem_info']."++".$currentconf['hdd_info']."++".$currentconf["img"]."++".$i;
-		}
-		else
-		{
-			$_SESSION['conf'.$i]["name"]=$name;	
-			echo $name."++".$checked."++".$nrcheck."++".$currentconf["id"]."++".$currentconf['cpu_info']."++".$currentconf['gpu_info']."++".$currentconf['disp_size']."++".$currentconf['disp_res']."++".$currentconf['mem_info']."++".$currentconf['hdd_info']."++".$currentconf["img"]."++".$i;
-		}
+		$result=mysqli_query($con,$sql); $currentconf["img"]="missing";
+		while($row=mysqli_fetch_assoc($result)){ if(strlen($row["submodel"])>21 && !preg_match("/\(.*\)/",$row["submodel"])){ $row["submodel"]=substr($row["submodel"],0,20)."."; } $name=$row["fam"]." ".$row["model"]."".$row["submodel"]; $currentconf["img"]=$row["img_1"]; }
+		mysqli_free_result($result); $_SESSION['conf'.$i]=$currentconf; $nrcheck++;
+		if($nrcheck<4){ $_SESSION['conf'.$i]["checked"]=1; $checked="checked='checked'"; } else { $_SESSION['conf'.$i]["checked"]=0; $checked=""; $nrcheck=4; }
+		if($k){ $_SESSION['conf'.$i]["name"]=$name." (".$k.")"; echo $name." (".$k.")++".$checked."++".$nrcheck."++".$currentconf["id"]."++".$currentconf['cpu_info']."++".$currentconf['gpu_info']."++".$currentconf['disp_size']."++".$currentconf['disp_res']."++".$currentconf['mem_info']."++".$currentconf['hdd_info']."++".$currentconf["img"]."++".$i; }
+		else   { $_SESSION['conf'.$i]["name"]=$name;        echo $name."++".$checked."++".$nrcheck."++".$currentconf["id"]."++".$currentconf['cpu_info']."++".$currentconf['gpu_info']."++".$currentconf['disp_size']."++".$currentconf['disp_res']."++".$currentconf['mem_info']."++".$currentconf['hdd_info']."++".$currentconf["img"]."++".$i; }
 	}
 }
 else
--- model/lib/php/query/chassis.php
+++ model/lib/php/query/chassis.php
@@ -1,47 +1,81 @@
 <?php
-if(isset($_GET['q'])){ $q=filter_var($_GET['q'], FILTER_VALIDATE_INT); if($q===FALSE){ $q=-1; } } else { $q=-1;}
-if($q>=0)
-{
-	require("../../../../etc/con_db.php");
-	require("../../../../etc/rates_conf.php");
-	mysqli_select_db($con,$global_notebro_db);
-	$sql="SELECT * FROM CHASSIS WHERE id = '".$q."'";
-	$result = mysqli_query($con,$sql);
-	
-	$rows = array();
-	while($r = mysqli_fetch_assoc($result)) 
-	{
-		foreach ($r as $key => $value) {
-			if (is_numeric($value)) {
-				$r[$key] += 0;          // cast numeric strings to numbers
-			}
-		}
-	
-		$rows[] = $r;
-		$rows[0]['made']=explode(",",$rows[0]['made']);
-		if(!empty($rows[0]['s_made'])){$rows[0]['made']=array_unique(array_merge($rows[0]['made'],explode(",",$rows[0]['s_made'])));}
-		$rows[0]['made']=implode(", ",$rows[0]['made']);
-		$rows[0]['msc']=str_replace(",", ", ",$rows[0]['msc']);
-		$rows[0]['pi']=str_replace(",", ", ",$rows[0]['pi']);
-		$rows[0]['keyboard']=str_replace(",", ", ",$rows[0]['keyboard']);
-		$rows[0]['vi']=str_replace(",", ", ",$rows[0]['vi']);
-		$rows[0]['color']=str_replace(",", ", ",$rows[0]['color']);
+if(isset($_GET['q'])){ 
+    $q = filter_var($_GET['q'], FILTER_VALIDATE_INT); 
+    if($q === FALSE){ 
+        $q = -1; 
+    } 
+} else { 
+    $q = -1;
+}
 
-		if(!$rows[0]['msc']) { $rows[0]['msc']="-"; }
-		if(!$rows[0]['pi']) { $rows[0]['pi']="-"; }
-		if(!$rows[0]['vi']) { if(stripos($rows[0]['pi'],"thunderbolt")!==FALSE) { $rows[0]['vi']="1 X mDP (Thunderbolt)"; } else { $rows[0]['vi']="-"; } }else { if(stripos($rows[0]['pi'],"thunderbolt")!==FALSE&&stripos($rows[0]['vi'],"DP")===FALSE) { $rows[0]['vi'].=", 1 X mDP (Thunderbolt)"; } }
-		if(!$rows[0]['color']) { $rows[0]['color']="-"; }
-		if(!$rows[0]['web']) { $rows[0]['web']="None"; }
-		if(!$rows[0]['touch']) { $rows[0]['touch']="Standard"; }
-		if(!$rows[0]['charger']) { $rows[0]['charger']="-"; } else { $rows[0]['charger']=str_replace(",", ",<br>",$rows[0]['charger']); }
-		//$rows[0]['price']=round($rows[0]['price'],2);
-		$rows[0]['price']=0;
-		$rows[0]['confrate'] = round($rows[0]['rating'],3)*$chassis_i/100;	
-	}
-$rows[0]['rating']=0;
-print json_encode($rows[0]);
-mysqli_close($con);
+if($q >= 0)
+{
+    require("../../../../etc/con_db.php");
+    require("../../../../etc/rates_conf.php");
+    mysqli_select_db($con, $global_notebro_db);
+
+    $sql    = "SELECT * FROM CHASSIS WHERE id = '".$q."'";
+    $result = mysqli_query($con,$sql);
+    
+    $rows = array();
+    while($r = mysqli_fetch_assoc($result)) 
+    {
+        foreach ($r as $key => $value) {
+            if (is_numeric($value)) {
+                $r[$key] += 0;          // cast numeric strings to numbers
+            }
+        }
+    
+        $rows[] = $r;
+
+        $rows[0]['made'] = explode(",", $rows[0]['made'] ?? '');
+        if (!empty($rows[0]['s_made'])) {
+            $rows[0]['made'] = array_unique(
+                array_merge($rows[0]['made'], explode(",", $rows[0]['s_made']))
+            );
+        }
+        $rows[0]['made'] = implode(", ", $rows[0]['made']);
+
+        // Prevent PHP 8.1+ deprecation notices (null → empty string)
+        $rows[0]['msc']      = str_replace(',', ', ', $rows[0]['msc']      ?? '');
+        $rows[0]['pi']       = str_replace(',', ', ', $rows[0]['pi']       ?? '');
+        $rows[0]['keyboard'] = str_replace(',', ', ', $rows[0]['keyboard'] ?? '');
+        $rows[0]['vi']       = str_replace(',', ', ', $rows[0]['vi']       ?? '');
+        $rows[0]['color']    = str_replace(',', ', ', $rows[0]['color']    ?? '');
+
+        if(!$rows[0]['msc']) { $rows[0]['msc']="-"; }
+        if(!$rows[0]['pi'])  { $rows[0]['pi']="-"; }
+        if(!$rows[0]['vi']) { 
+            if(stripos($rows[0]['pi'],"thunderbolt")!==FALSE) { 
+                $rows[0]['vi']="1 X mDP (Thunderbolt)"; 
+            } else { 
+                $rows[0]['vi']="-"; 
+            } 
+        } else { 
+            if(stripos($rows[0]['pi'],"thunderbolt")!==FALSE && stripos($rows[0]['vi'],"DP")===FALSE) { 
+                $rows[0]['vi'].=", 1 X mDP (Thunderbolt)"; 
+            } 
+        }
+        if(!$rows[0]['color']) { $rows[0]['color']="-"; }
+        if(!$rows[0]['web'])   { $rows[0]['web']="None"; }
+        if(!$rows[0]['touch']) { $rows[0]['touch']="Standard"; }
+
+        if(!$rows[0]['charger']) { 
+            $rows[0]['charger']="-"; 
+        } else { 
+            $rows[0]['charger']=str_replace(",", ",<br>",$rows[0]['charger']); 
+        }
+        //$rows[0]['price']=round($rows[0]['price'],2);
+        $rows[0]['price']=0;
+        $rows[0]['confrate'] = round($rows[0]['rating'],3)*$chassis_i/100;  
+    }
+
+    $rows[0]['rating']=0;
+    print json_encode($rows[0]);
+    mysqli_close($con);
 }
 else
-{ header('X-PHP-Response-Code: 204', true, 204); }
+{
+    header('X-PHP-Response-Code: 204', true, 204);
+}
 ?>
